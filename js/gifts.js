64a42edfe5d552c53809c05a273da42c807f8e7b
Omar-zoubi <o.fandi02@gmail.com>
2021-02-27 18:19:13 +0200

    aa

--- js/gifts.js
+++ js/gifts.js
@@ -3,6 +3,7 @@
 let giftForm=document.getElementById('giftSide');
 
 let selectType=document.getElementById('type');
+let giftSide= document.getElementById('category');
 
 function payment(){
   let lable1=document.createElement('label');
@@ -63,21 +64,52 @@ function inKind (){
   input3.name = 'address';
 }
 
-// let firstOp = document.getElementById('in-kind');
-// let secandOp = document.getElementById('financial');
 
-// firstOp.addEventListener('click', action);
-// secandOp.addEventListener('click', action);
-// function action (event){
-//   console.log(firstOp.value);
-//   if (firstOp) {
-//     payment();
-//     console.log(firstOp);
-//   } else if (secandOp){
-//     inKind ();
-//   }
-//   //selectType.removeEventListener('click', action);
-// }
-selectType.addEventListener('select',function (){if (selectType.id=='in-kind'){payment();}   });
+selectType.addEventListener('change',action);
 
+
+function action (event){
+  
+
+  if (selectType.value=='in-kind')
+  {   
+
+     inKind ();
+  }
+  else if(selectType.value== 'financial')
+  {
+    payment();
+  
+  }
+
+}
+
+giftSide.addEventListener('change',function(){
+  if (giftSide.value === 'cancer')
+  {
+    let img =document.createElement('img');
+img.src='https://upload.wikimedia.org/wikipedia/commons/6/6f/Al-Hussain_Cancer_Center.jpg'
+document.getElementById('b1').appendChild(img); 
+
+  }else if(giftSide.value=='refugees')
+  {
+    let img =document.createElement('img');
+    img.src='https://www.madania.tn/wp-content/uploads/2018/02/MainRefugee.jpg'
+    document.getElementById('b1').appendChild(img); 
+    
+  }else if (giftSide.value=='orphans')
+  {
+    let img =document.createElement('img');
+    img.src='https://2.bp.blogspot.com/-Ftzno8ran0M/W_hQJ3BEY8I/AAAAAAAACbg/550P0Y7cHXc0BahTp4PhJOex-1e9OTK0gCLcBGAs/s1600/f98dcce921c21f0cb2031d9624a0a7e4_400x400.jpeg'
+    document.getElementById('b1').appendChild(img); 
+    
+  }
+  else if (giftSide.value=='poor')
+  {let img =document.createElement('img');
+  img.src='http://almasalah.com/MediaStorage/GalleryImages/129782.jpg?width=750&&height=375'
+  document.getElementById('b1').appendChild(img); 
+  
+
+  }
+});
 

eb3bed57bfddbb700f1f371cc1249cc4a678ee79
judyal-taweel <judybataineh@gmail.com>
2021-03-03 14:10:42 +0200

    editing event page

--- js/events.js
+++ js/events.js
@@ -1,15 +1,28 @@
 'use strict';
 
-let eventForm = document.getElementById('eventForm');
 
+let eventdiv = document.getElementById('eventdiv');
+
+let eventForm = document.getElementById('eventForm');
 let submit = document.getElementById('submit');
+submit  = document.querySelector("button");
 submit.addEventListener('click', thanks);
 let paragraph2 = document.getElementById('paragraph2');
 console.log(eventForm);
 function thanks(event){
   event.preventDefault();
 
-  paragraph2.textContent = `Thank you ${eventForm.name.value} , for volunteering in ${eventForm.eventsSelect.value} we will contact you very soon `;
+  
+
+  paragraph2.textContent = `Thank you ${eventForm.name.value} , for volunteering in ${eventForm.eventsSelect.value} we will contact you very soon. `;
+}
+
+
+
+submit.addEventListener("click", active);
+
+function active() {
+  submit.classList.toggle("is_active");
 }
 
 
